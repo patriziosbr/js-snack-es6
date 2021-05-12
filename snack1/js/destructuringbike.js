ebfff84d1d7c1780b2c9e2c8887735101fde239d
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-05-12 14:39:24 +0200

    start 1

---
+++ snack1/js/destructuringbike.js
@@ -0,0 +1,25 @@
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal.
+
+
+var biciclette = [
+    
+    {
+        nome: "Raleghi",
+        peso: 9
+    },
+    {
+        nome: "Rock Ride",
+        peso: 6
+    },
+    {
+        nome: "bianchi",
+        peso: 12
+    },
+    {
+        nome: "pursuit",
+        peso: 8
+    }
+
+];
+
+console.log(biciclette);
